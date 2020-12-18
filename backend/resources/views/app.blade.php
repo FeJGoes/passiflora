a1d7745562e0f43b08422fb45ff1400a55980f54
Felipe J. de Góes <felipe.92goes@gmail.com>
2020-12-18 18:52:22 -0300

    reset project 2

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -3,10 +3,11 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Laravel</title>
+  <link rel="stylesheet" href="{{mix('style.css')}}">
+  <title>passiflora</title>
 </head>
 <body>
-  <div id="app"></div>
-  <script src="{{ mix('js/app.js') }}"></script>
+
+  <script src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
